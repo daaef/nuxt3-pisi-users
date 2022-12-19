f318899124a79e232eac40a951eab5a4c7634265
Afekhide Gbadamosi <realafe21@gmail.com>
2022-12-20 00:10:09 +0100

    dropdown created

--- components/ROCurrency.vue
+++ components/ROCurrency.vue
@@ -1,76 +1,15 @@
 <template>
-  <el-menu
-      :default-active="$route.fullPath"
-      class="sidebar"
-      @open="handleOpen"
-      @close="handleClose"
-      router
-  >
-    <div class="header-sidebar">
-      <img src="/auth-logo.png" alt="" />
-    </div>
-    <div class="sidebar--grid">
-      <div class="">
-
-        <el-sub-menu index="1">
-          <template #title>
-            <div class="flex flex-col line-less justify-center">
-              <h4>Matthew Ero</h4>
-              <div class="text-gray-400">
-                <i class="iconly-Location mr-1 text-xl icli" />
-                <!--                  <el-icon><location /></el-icon>-->
-                <span>Nigeria</span>
-              </div>
-            </div>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item class="mt-10" index="/">
-          <i class="iconly-Activity mr-2 text-xl icli" />
-          <span>Transactions</span>
-        </el-menu-item>
-      </div>
-      <div class="">
-        <el-menu-item class="mb-3" index="/2">
-          <i class="iconly-Swap mr-2 text-xl icli" />
-          <span>Exchange</span>
-        </el-menu-item>
-        <el-menu-item index="/3">
-          <i class="iconly-Time-Circle mr-2 text-xl icli" />
-          <span>History</span>
-        </el-menu-item>
-      </div>
-      <div class="">
-        <el-menu-item index="/4">
-          <i class="iconly-Info-Circle mr-2 text-xl icli" />
-          <span>Help Center</span>
-        </el-menu-item>
-        <el-menu-item index="6" class="non-default">
-          <i class="iconly-Logout mr-2 text-xl icli" />
-          <span>Logout</span>
-        </el-menu-item>
-      </div>
-    </div>
-  </el-menu>
+  <span class="a-icon flex items-center">
+    <i :class="`cf cf-${icon.toLowerCase()} text-2xl mr-2`" />
+    {{ name }}
+  </span>
 </template>
 
 <script lang="ts" setup>
-  const handleOpen = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-  }
-  const handleClose = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-  }
+  defineProps({
+    icon: String,
+    name: String
+  })
 </script>
 
 <style scoped>
--- components/ROStack.vue
+++ components/ROStack.vue
@@ -3,19 +3,55 @@
     <div class="main--stack">
       <div class="exchange--inputs">
         <div class="mt-4">
-          <el-input
+          <el-form
+              label-position="top"
+              :model="myForm"
+          >
+          <el-form-item
+              label="Sell">
+            <el-input
               v-model="input3"
               placeholder="Please input"
               class="input-with-select"
           >
+            <template #prefix>
+              <i :class="`cf cf-${selected.toLowerCase()} text-xl mr-2`" />
+            </template>
             <template #prepend>
-              <el-select v-model="select" placeholder="Select" style="width: 115px">
-                <el-option label="Restaurant" value="1" />
-                <el-option label="Order No." value="2" />
-                <el-option label="Tel" value="3" />
+              <el-select v-model="selected" placeholder="Select" style="width: 80px">
+                <el-option
+                    v-for="coin in store.coins"
+                    :label="coin.name"
+                    :key="coin.id"
+                    :value="coin.name"
+                >
+                  <ROCurrency :icon="coin.name" :name="coin.name" />
+                </el-option>
               </el-select>
+<!--              <el-dropdown>
+                <span class="el-dropdown-link">
+                  hey<el-icon class="el-icon&#45;&#45;right"><arrow-down /></el-icon>
+                </span>
+                <template #dropdown>
+                  <el-dropdown-menu>
+                    <el-dropdown-item>
+                      <ROCurrency icon="btc" name="BTC" />
+                    </el-dropdown-item>
+                    <el-dropdown-item>
+                      <ROCurrency icon="btc" name="BTC" />
+                    </el-dropdown-item>
+                    <el-dropdown-item>
+                      <ROCurrency icon="btc" name="BTC" />
+                    </el-dropdown-item>
+                    <el-dropdown-item>Action 4</el-dropdown-item>
+                    <el-dropdown-item>Action 5</el-dropdown-item>
+                  </el-dropdown-menu>
+                </template>
+              </el-dropdown>-->
             </template>
           </el-input>
+          </el-form-item>
+          </el-form>
         </div>
         <el-button
             class="mt-4 ml-5"
@@ -34,14 +70,26 @@
 </template>
 
 <script lang="ts" setup>
+import {useStore} from "~/stores";
+import {watchEffect} from "@vue/runtime-core";
+
+  const store = useStore()
   const handleOpen = (key: string, keyPath: string[]) => {
     console.log(key, keyPath)
   }
   const handleClose = (key: string, keyPath: string[]) => {
     console.log(key, keyPath)
   }
+  const myForm = ref({})
+  const input3 = ref('')
+  const selected = ref('')
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  .cf.cf-btc {
+    color: #f2a900;
+  }
+  .el-input-group--prepend .el-input-group__prepend .el-select .el-input .el-input__wrapper {
+    box-shadow: none !important;
+  }
 </style>
